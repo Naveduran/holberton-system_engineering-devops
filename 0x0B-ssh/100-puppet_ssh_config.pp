f2c14b5245f1be7b1555c2acaecc7b6f4d1da6e2
Naveduran <naveduran@gmail.com>
2021-08-16 06:37:35 +0000

    using tabs

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # u sing Puppet to make changes to our configuration file of a server
 
-$s = "     IdentityFile ~/.ssh/holberton\n     PasswordAuthentication no\n"
+$s = "\tIdentityFile ~/.ssh/holberton\n \tPasswordAuthentication no\n"
 
 file_line { 'Add two lines to the ssh config file':
   ensure    => present,

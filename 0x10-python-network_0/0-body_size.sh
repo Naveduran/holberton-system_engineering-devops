746affd16ba3f0c0fbc8387bdf7875c601ac1b47
Naveduran <naveduran@gmail.com>
2021-08-20 03:45:35 +0000

    [Fix] only one line comment

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
+# request an URL and displays the size of the body of the response
 curl "$1"  --silent -output A | cat A | wc -m

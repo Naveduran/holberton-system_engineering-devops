446badcceafc625ccc9d20afa7c351bc0d82b3a4
Naveduran <naveduran@gmail.com>
2021-08-28 00:46:50 +0000

    [Fix] minify

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,26 +8,12 @@ package { 'nginx':
   name     => 'nginx',
   provider => 'apt',
 }
-# Create redirect page
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => "\n\tlocation /redirect_me {\n\t\treturn 301 https://www.google.com;\n\t}\n",
-}
 # Add header
 file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => "\n\t\tadd_header X-Served-By $HOSTNAME;\n",
-}
-
-# Create html fake file
-file {'index.html':
-  ensure  => 'present',
-  content => 'Holberton School',
-  path    => '/var/www/html/index.html',
+  line   => "\tadd_header X-Served-By ${HOSTNAME};",
 }
 # Nginx is running
 exec {'restart':

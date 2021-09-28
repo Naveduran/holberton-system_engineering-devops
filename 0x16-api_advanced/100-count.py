486f9fad5b8cd6d0c1a2171b5ba3ce59362bbc61
Naveduran <naveduran@gmail.com>
2021-09-28 06:23:36 +0000

    [Add] better performance

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,10 +4,12 @@ import requests
 
 
 def to_text(hot_dict):
-    """printing function """
-    if len(hot_dict) == 0:
-        return ''
+    """ Input: A dictionary.
+    Sort keys alphabetically and values in descending order.
+    Then, print the dictionary"""
     text = ''
+    if len(hot_dict) == 0:
+        return text
     ordered_by_alpha = sorted(hot_dict.items(),
                               key=lambda tup: tup[0],
                               reverse=False)
@@ -15,7 +17,7 @@ def to_text(hot_dict):
                                key=lambda tup: tup[1],
                                reverse=True)
     for key in ordered_by_values:
-        value = hot_dict.get(key[0])
+        value = hot_dict[key[0]]
         text += key[0] + ': ' + str(value) + '\n'
     print(text, end='')
 
@@ -30,9 +32,7 @@ def count_words(subreddit, word_list, after='', hot_dict={}):
     url = 'https://www.reddit.com/r/{}/hot.json?after={}&limit=1'.format(
         subreddit, after)
 
-    headers = {
-        'User-Agent':
-        'Nat'}
+    headers = {'User-Agent': 'Nat'}
 
     r = requests.get(url, headers=headers, allow_redirects=False)
 
@@ -40,15 +40,14 @@ def count_words(subreddit, word_list, after='', hot_dict={}):
     if r.status_code != 200:
         return
 
-    # Get the data from the json
-    data = r.json().get('data')
+    data = r.json()['data']
 
     # If there are no more elements to check --> print!
-    if len(data.get('children')) == 0:
+    if len(data['children']) == 0:
         return to_text(hot_dict)
 
-    # Get the tittle of this post and turn to lower case,
-    title = data.get('children')[0].get('data').get('title').lower()
+    # Get the tittle of this post and turn to lower case
+    title = data['children'][0]['data']['title'].lower()
 
     # Split words of the title, put in a list to search easier
     title_words = title.split()
@@ -57,11 +56,10 @@ def count_words(subreddit, word_list, after='', hot_dict={}):
     for word in word_list:
         if title_words.count(word):
             if word in hot_dict:
-                number = hot_dict.get(word) + title_words.count(word)
-                hot_dict.update({word: number})
+                hot_dict[word] += title_words.count(word)
             else:
-                hot_dict.update({word: title_words.count(word)})
+                hot_dict[word] = title_words.count(word)
 
     # Call this recursive function for the next reddit post
-    after = data.get('after')
+    after = data['after']
     count_words(subreddit, word_list, after, hot_dict)

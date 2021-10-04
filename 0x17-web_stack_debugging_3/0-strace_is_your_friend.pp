04843473328cee4c6c835d96287653b6f25e161b
Naveduran <naveduran@gmail.com>
2021-10-04 16:57:01 +0000

    [Add] puppet file for debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fix a typo in the configuration files
+# Fix a typo in the configuration file s
 exec{'sed':
   path    => '/bin',
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"

c395e4b7c4d96638b60ef8a0e7bf7f7bd4c7d670
Naveduran <naveduran@gmail.com>
2021-10-07 22:44:55 +0000

    [Add] restart service and puppet lint correction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,8 @@ exec{'sed':
   path    => '/bin',
   command => "sed -i '5s/.*/ULIMIT=\"-n 2000\"/' /etc/default/nginx"
 }
+
+exec{'sed':
+  path    => '/bin',
+  command => 'sudo service nginx restart'
+}

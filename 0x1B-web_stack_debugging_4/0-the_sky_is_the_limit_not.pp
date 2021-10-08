21003591e4875356291e426515ac685d290470f5
Natalia Vera Duran <naveduran@gmail.com>
2021-10-07 21:15:18 -0500

    [Add] reduce lines

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,5 @@
-# Allow multiple petitons at the sametiem in a nginx server
-exec{'sed':
-  path    => '/bin',
-  command => 'sed -i s/15/2000/ /etc/default/nginx'
-}
-exec{'restart':
-  path    => '/bin',
-  command => 'service nginx restart'
+# Allow multiple request at the same time in a nginx server
+exec{'limit':
+  path    => '/usr/bin:/bin',
+  command => 'sed -i s/15/2000/ /etc/default/nginx; service nginx restart'
 }
